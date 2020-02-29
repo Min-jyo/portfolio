18d268987e054e2e09e8d8f01adf87ede095b6f4
min-jyo <minjyo95@gmail.com>
2020-02-29 20:11:32 +0900

    웹프스 유저의 나이 필드 추가

--- app/many_to_one/models/recursive.py
+++ app/many_to_one/models/recursive.py
@@ -6,6 +6,7 @@ class WPSUser(models.Model):
         'self', on_delete=models.SET_NULL,
         related_name='student_set', null=True)
     name = models.CharField(max_length=20)
+    age = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
